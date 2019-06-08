4c19ff1cd7dc4f829e67ebcd6b2890fbd294cdad
pmli <kamisan118@gmail.com>
2019-06-08 19:30:23 +0800

    fix white layout problem

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="create">
     <v-app>
-      <v-navigation-drawer app></v-navigation-drawer>
+      <!-- <v-navigation-drawer app></v-navigation-drawer> -->
       <v-toolbar app color="brown darken-4" dark>
         <v-toolbar-side-icon></v-toolbar-side-icon>
         <v-toolbar-title>Create new record</v-toolbar-title>
@@ -9,7 +9,7 @@
         <v-spacer></v-spacer>
         <v-btn flat>SIGN IN</v-btn>
         <v-btn color="brown lighten-3">JOIN</v-btn>
-      </v-toolbar&gt;&lt;/template>
+      </v-toolbar>
       <v-content>
         <v-container fluid>
           <v-layout align-center justify-center column fill-height/>
