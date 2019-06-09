166ae46debce0db9ff4a9f9da25eebeaa282c174
KaKeiMei <hqm237594742@gmail.com>
2019-06-09 10:47:16 +0900

    fix address

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,13 @@
 <template lang="html">
   <v-layout align-center justify-end wrap id="login">
-    <v-flex shrink id="address">
-      <!-- <v-textarea
-        readonly
-        auto-grow
-        rows=1
-        single-line
-        v-model="address"
-      /> -->
-      <v-btn flat disabled small>{{ address }}</v-btn>
+    <v-flex  xs4 >
+      <v-card flat color="transparent">
+        <v-card-text>
+          <p class="text-xs-right">
+            {{ address }}
+          </p>
+        </v-card-text>
+      </v-card>
     </v-flex>
     <ConnectButton />
   </v-layout>
